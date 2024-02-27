6bb23f1bb68b0bc9f96ffc9462f2ddd394ad4de0
Galina Dei <gal-dej@yandex.ru>
2024-02-27 15:28:37 +0300

    Start

--- Task_one.py
+++ Task_one.py
@@ -1,2 +1,12 @@
 # Напишите функцию, которая принимает на вход строку - абсолютный путь до файла.
 # Функция возвращает кортеж из трёх элементов: путь, имя файла, расширение файла.
+
+def pass_parsing(the_pass: str)->tuple:
+    *a, b = the_pass.split("\\")
+    a = '\\'.join(a)
+    *c, d = b.split('.')
+    c = ' '.join(c)
+    return (a, c, d)
+
+res = pass_parsing("E:\Информационные технологии\Tecknological_specialization\Data_ingineer\Python\Seminar 5. Итераторы и генераторы.mp4")
+print(*res, sep='\n')
--- Task_three.py
+++ Task_three.py
@@ -1,1 +1,14 @@
 # Создайте функцию генератор чисел Фибоначчи
+def fibonacci_gen(n):
+    num = 1
+    num1 = 0
+    num2 = 1
+    for i in range(n):
+        yield num
+        num = num1 + num2
+        num1 = num2
+        num2 = num
+
+
+for i in fibonacci_gen(10):
+    print(i)
